57cf4d9891e8b053e5e536655645ce5b0dd0ff26
Sakif Ahmed <ahmed.sakif225@gmail.com>
2025-11-13 18:03:52 +0600

    Feature section done

--- src/Components/Feature/Feature.jsx
+++ src/Components/Feature/Feature.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import FeatureCard from './FeatureCard';
 import { ChartIcon, PlusIcon, SearchIcon } from './FeatureIconsSVG';
-
-
-
+import { Link } from 'react-router';
 
 
 const Feature = () => {
   return (
     <section className="my-20 bg-base-100 ">
       <div className="container max-w-[1536px] mx-auto px-4">
-        
+
         {/* Section Header */}
         <div className="text-center mb-12">
           <h2 className="text-4xl font-bold mb-4">How It Works</h2>
@@ -19,28 +17,34 @@ const Feature = () => {
             impact in the community.
           </p>
         </div>
-        
+
         {/* Responsive Grid for the cards */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          
-          <FeatureCard
-            icon={<SearchIcon />}
-            title="Discover Local Events"
-            description="Find volunteer opportunities right in your neighborhood. Filter by category, date, and location to join events that matter to you."
-          />
-          
-          <FeatureCard
-            icon={<PlusIcon />}
-            title="Lead Your Initiative"
-            description="Have an idea for a clean-up drive or a donation camp? Post your own event in minutes and invite others to join your cause."
-          />
-          
+
+          <Link to='/events'>
+            <FeatureCard
+              icon={<SearchIcon />}
+              title="Discover Local Events"
+              description="Find volunteer opportunities right in your neighborhood. Filter by category, date, and location to join events that matter to you."
+            />
+          </Link>
+
+          <Link to="event/create">
+            <FeatureCard
+              icon={<PlusIcon />}
+              title="Lead Your Initiative"
+              description="Have an idea for a clean-up drive or a donation camp? Post your own event in minutes and invite others to join your cause."
+            />
+          </Link>
+
+          <Link to='/event/manage'>
           <FeatureCard
             icon={<ChartIcon />}
             title="Track Your Impact"
             description="See a complete history of all the events you've joined and organized. Watch your contribution to a better tomorrow grow over time."
           />
-          
+          </Link>
+
         </div>
       </div>
     </section>
